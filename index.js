8927881311493508aa3c2967852b0456ca309801
Ye Myint Kyaw <yemyintkyaw241220@gmail.com>
2022-12-20 11:03:05 +0700

    change the slidebar appearance

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const openIcon = document.querySelector(".openIcon");
 const closeIcon = document.querySelector(".closeIcon");
 const slideBar = document.querySelector(".slideBar");
 openIcon.addEventListener("click", function () {
-  slideBar.style.top = "0";
+  slideBar.style.width = "100vw";
+
   document.body.style.overflow = "hidden";
   window.scrollTo(0, 0);
 });
 closeIcon.addEventListener("click", function () {
-  slideBar.style.top = "-100vh";
-
+  slideBar.style.width = "0";
   document.body.style.overflowY = "scroll";
 });
